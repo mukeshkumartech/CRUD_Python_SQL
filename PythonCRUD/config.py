76ba9a2fcda724e3ef5b125a9b41d998495e4313
Mukesh Kumar <mahi.aptech@live.com>
2019-01-10 00:53:57 +0530

    Add project files.

---
+++ PythonCRUD/config.py
@@ -0,0 +1,12 @@
+
+# SQL Server Database Connection Properties
+DATABASE_CONFIG ={
+    'Driver': 'SQL Server',
+    'Server': 'DESKTOP-QQ95JAD',
+    'Database': 'Test',
+    'UID': 'sa',
+    'Password': 'xxxxxx'
+    }
+
+
+
